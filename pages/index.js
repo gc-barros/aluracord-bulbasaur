b6234c8c27dc60428f5b2908c25c4db498a987b3
Gabriel Barros <super.gcb18@gmail.com>
2022-01-28 13:47:14 -0300

    Index update

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ export default function PaginaInicial() {
             as="form"
             onSubmit={function (event) {
               event.preventDefault();
-              roteamento.push("/chat");
+              roteamento.push(`/chat?username=${username}`);
             }}
             styleSheet={{
               display: "flex",
